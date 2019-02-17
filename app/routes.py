6d7b5ccb5562deed131601ec30d3cbbea92d981c
nandujkishor <nandujkishor@gmail.com>
2019-02-17 19:36:49 +0530

    Changed requirements

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ import os
 import json
 import datetime
 from flask import render_template, flash, redirect, request, url_for, jsonify
-from app import app, db, login, mail
+from app import app, db
 from config import Config
 from werkzeug.utils import secure_filename
 from werkzeug.urls import url_parse
