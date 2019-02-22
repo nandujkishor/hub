cc1a1db50c362dff197a30162ada3a8cb2a8bf04
nandujkishor <nandujkishor@gmail.com>
2019-02-22 23:56:04 +0530

    T

--- app/farer.py
+++ app/farer.py
@@ -5,7 +5,6 @@ from flask import render_template, flash, redirect, request, url_for, jsonify
 from app import app, db, api
 from app.models import User, Staff
 from config import Config
-
 from werkzeug.utils import secure_filename
 from werkzeug.urls import url_parse
 from flask_restplus import Resource, Api
