95922b1be3b4097bece8d221fe3c0558ef2a4d38
Aswanth <aswanth@am.students.amrita.edu>
2019-03-14 16:01:53 +0530

    C

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,14 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restplus import Api
+from flask_qrcode import QRcode
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+QRcode(app)
+
 api = Api(app, 
     version='0.8.1',
     title="Switch API", 
